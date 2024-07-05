f59e0b014c1ff18e861517d30addf0783f2ff101
pnikolaid <nikolaid@umd.edu>
2024-07-05 17:44:29 +0200

    Simplified ping test

--- start_quectel.sh
+++ start_quectel.sh
@@ -7,11 +7,6 @@ ip route add 192.168.70.128/26 via 192.168.225.1
 ip route add 192.168.70.129/32 via 192.168.225.1
 
 # Ping the server (send only 1 packet) and redirect output to /dev/null
-ping -c 1 192.168.70.129 > /dev/null 2>&1
+ping -c 1 192.168.70.129
 
-# Check the exit status of the ping command
-if [ $? -eq 0 ]; then
-    echo "Ping to $SERVER was successful."
-else
-    echo "Ping to $SERVER failed."
-fi
+
