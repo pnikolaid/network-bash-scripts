17a120b23f7c80bb9bc2b2964a00f4d4ee59d8c6
pnikolaid <nikolaid@umd.edu>
2024-07-09 19:51:49 +0200

    nfixed cleanup log

--- cleanup_logs.sh
+++ cleanup_logs.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-# Directory to clean
-DIR="/tmp"
-
-# File patterns to clean
-PATTERNS=("sent*" "recv*" "iperf3*")
-
-# Iterate over each pattern and truncate matching files
-for pattern in "${PATTERNS[@]}"; do
-    # Use truncate command to clear file contents
-    find "$DIR" -type f -name "${pattern}" -exec truncate -s 0 {} +
-done
-
-echo "Cleanup complete: Cleared contents of files starting with sent, recv, or iperf3 in /tmp."
+cd /tmp
+truncate -s 0 recv*
+truncate -s 0 sent*
+truncate -s 0 iperf3*
--- delete_logs.sh
+++ delete_logs.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-# Directory to clean
-DIR="/tmp"
-
-# File patterns to remove
-PATTERNS=("recv" "sent" "iperf3")
-
-# Iterate over each pattern and remove matching files
-for pattern in "${PATTERNS[@]}"; do
-    # Find files matching the pattern and delete them
-    find "$DIR" -type f -name "${pattern}*" -exec rm -f {} \;
-done
+cd /tmp
+rm recv*
+rm sent*
+rm iperf3*
